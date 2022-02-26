c989e784b20ec70bc078129dafdcb4ad89f663b4
smirnovaalyona <99326020+smirnovaalyona@users.noreply.github.com>
2022-02-26 21:43:44 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,16 +5,45 @@
 
 bool checkPrime(uint64_t value) {
 // вставьте код функции
+if (value == 2) return true;
+if (value < 2) return false;
+for (uint64_t i = 2; i <= sqrt(value); i++) {
+if (!(value%i)) {
+return false; 
+}
+}
+return true;
 }
 
 uint64_t nPrime(uint64_t n) {
 // вставьте код функции
+uint64_t t_num = 0;
+for (uint64_t i = 2; i < UINT64_MAX; i++) {
+if (checkPrime(i)) {
+t_num++;
+}
+if (n == t_num) {
+return i;
+}
+}
 }
 
 uint64_t nextPrime(uint64_t value) {
 // вставьте код функции
+for (uint64_t i = value + 1; i < UINT64_MAX; i++) {
+if (checkPrime(i)) {
+return i;
+}
+}
 }
 
 uint64_t sumPrime(uint64_t hbound) {
 // вставьте код функции
+uint64_t summa = 0;
+for (int i = 2; i < hbround; i++) {
+if (checkPrime(i)) {
+summa = summa + i;
+}
+}
+return summa;
 }
