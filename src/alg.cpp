0b4f1cc7e04e5e6d11209cfeda437ef171080011
smirnovaalyona <99326020+smirnovaalyona@users.noreply.github.com>
2022-06-09 18:19:34 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,47 +6,47 @@
 
 bool checkPrime(uint64_t value) {
 // вставьте код функции
-if (value < 2) return false;
-if (value == 2) return true;
-for (uint64_t i = 2; i <= sqrt(value); i++) {
-if (!(value % i)) {
-return false;
-}
-}
-return true;
+  if (value < 2) return false;
+  if (value == 2) return true;
+  for (uint64_t m = 2; m <= sqrt(value); m++) {
+    if (!(value % m)) {
+      return false;
+    }
+  }
+  return true;
 }
 
 uint64_t nPrime(uint64_t n) {
 // вставьте код функции
-uint64_t  t_num = 0;
-for (uint64_t i = 2; i < UINT64_MAX; i++) {
-if (checkPrime(i)) {
-t_num++;
-}
-if (n == t_num) {
-return i;
-}
-}
-return 0;
+  uint64_t  t_numb = 0;
+  for (uint64_t m = 2; m < UINT64_MAX; m++) {
+    if (checkPrime(m)) {
+      t_numb++;
+    }
+    if (n == t_numb) {
+      return m;
+    }
+  }
+  return 0;
 }
 
 uint64_t nextPrime(uint64_t value) {
 // вставьте код функции
-for (uint64_t i = value+1; i < UINT64_MAX; i++) {
-if (checkPrime(i)) {
-return i;
-}
-}
-return 0;
+  for (uint64_t m = value+1; m < UINT64_MAX; m++) {
+    if (checkPrime(m)) {
+      return m;
+    }
+  }
+  return 0;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
 // вставьте код функции
-uint64_t summa = 0;
-for (int i = 2; i < hbound; i++) {
-if (checkPrime(i)) {
-summa = summa + i;
-}
-}
-return summa;
+  uint64_t sum = 0;
+  for (int m = 2; m < hbound; m++) {
+    if (checkPrime(m)) {
+      sum = sum + m;
+    }
+  }
+  return sum;
 }
